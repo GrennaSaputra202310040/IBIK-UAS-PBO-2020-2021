afd645ee34222851b4952e0dcdfa185f4b110286
GrennaSaputra202310040 <92084655+GrennaSaputra202310040@users.noreply.github.com>
2022-01-19 10:43:21 +0700

    Add files via upload

---
+++ UAS/soal/tujuh/sampai/sepuluh/app_library/src/main/java/com/ibik/library/app/login/CallbackResultSet.java
@@ -0,0 +1,23 @@
+package com.ibik.library.app.login;
+
+public class CallbackResultSet {
+    private boolean Response;
+    private String Label;
+
+    public boolean getResponse() {
+        return Response;
+    }
+
+    public void setResponse(boolean response) {
+        Response = response;
+    }
+
+    public String getLabel() {
+        return Label;
+    }
+
+    public void setLabel(String label) {
+        Label = label;
+
+    }
+}
